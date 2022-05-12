c78e8ee74ab65dc9cbb5441a72d4dfe89adad85f
xiaoznz <779740337@qq.com>
2022-05-12 17:10:35 +0800

    从vue3转到tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import {defineComponent, ref} from "vue";
+
+export const App = defineComponent({
+    setup(){
+        const count = ref(0);
+        const onClick = ()=>{
+            count.value += 1;
+        }
+
+        return ()=>(<>
+            <div>
+                {count.value}
+            </div>
+            <button onClick={onClick}>123</button>
+        </>)
+    }
+})
